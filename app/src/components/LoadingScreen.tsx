b9ed41b25f82518a7b6c72431143a29548d54b24
Michelle Rosenthal <127794503+mtrinhsenthal@users.noreply.github.com>
2025-11-24 20:12:05 -0800

    style loading screen

--- app/src/components/LoadingScreen.tsx
+++ app/src/components/LoadingScreen.tsx
@@ -16,47 +16,19 @@ export default function LoadingPage({ onFinishLoading }: LoadingPageProps) {
   }, [onFinishLoading]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen px-8 text-center">
-      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-gray-800"></div>
 
-      <p className="font-semibold mt-6 leading-relaxed">
-        Smart Suggestions will highlight habits you might want to track — helping you
-        discover what you don’t know yet!
-      </p>
+    <div className="flex items-center justify-center min-h-screen px-6">
+        <div className="bg-gradient-to-r from-blue-50 to-green-50 rounded-2xl p-6 border border-blue-200 shadow-md max-w-md mx-auto text-center">
+            <h1 className="text-sm font-bold text-[#312E81] mb-2">
+            Loading your dashboard based on your preferences...
+            </h1>
+            <p className="text-xs text-[#4C4799] mt-4 leading-relaxed">
+            Smart Suggestions will highlight habits you might want to track — helping you
+            discover what you don’t know yet!
+            </p>
+        </div>
     </div>
+
+    
   );
 }
-
-// "use client";
-
-// import { useEffect } from "react";
-
-// interface LoadingPageProps {
-//   onFinishLoading: () => void;
-// }
-
-// export default function LoadingPage({ onFinishLoading }: LoadingPageProps) {
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//       onFinishLoading();
-//     }, 2000);
-
-//     return () => clearTimeout(timer);
-//   }, [onFinishLoading]);
-
-//   return (
-//     <div className="flex flex-col items-center justify-center h-screen px-8 text-center">
-
-//       {/* BIG CLEAR LOADING SPINNER */}
-//       <div className="relative w-14 h-14">
-//         <div className="absolute inset-0 rounded-full border-4 border-gray-300" />
-//         <div className="absolute inset-0 rounded-full border-4 border-[#37A6B2] border-t-transparent animate-spin" />
-//       </div>
-
-//       <p className="text-xl font-semibold mt-8 leading-relaxed max-w-md">
-//         Smart Suggestions will highlight habits you might want to track —
-//         helping you discover useful things you haven’t thought of yet.
-//       </p>
-//     </div>
-//   );
-// }
